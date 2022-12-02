ecd68bf792b1d335e4efdb3872a7b20a814fd62e
univ <fcsnwu@163.com>
2022-12-02 11:29:28 +0800

    spring-boot:run加上远程调试参数

--- src/main/java/com/univ/initializer/InitializerApplication.java
+++ src/main/java/com/univ/initializer/InitializerApplication.java
@@ -1,10 +1,16 @@
 package com.univ.initializer;
 
+import com.alibaba.fastjson.JSONObject;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
@@ -18,8 +24,23 @@ import org.springframework.scheduling.annotation.EnableAsync;
 //@MapperScan({"com.univ.initializer.mapper"})
 @EnableCaching
 @EnableAsync
+@Slf4j
 public class InitializerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(InitializerApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication
+                .run(InitializerApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application initializer is running! Access URLs:\n\t" +
+                "注意端口替换成容器的端口(如有)\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "swagger-ui: \thttp://" + ip + ":" + port + path + "/swagger-ui.html\n\t" +
+                "Doc: \t\thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "active profile: \t\t" + JSONObject.toJSONString(env.getActiveProfiles()) + "\n" +
+                "----------------------------------------------------------");
     }
 }
