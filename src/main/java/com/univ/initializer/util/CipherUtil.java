3a4749c33a1df60ebe32935d5acb01cdea7914eb
univ <fcsnwu@163.com>
2023-02-27 11:23:24 +0800

    CipherUtil补充

--- src/main/java/com/univ/initializer/util/CipherUtil.java
+++ src/main/java/com/univ/initializer/util/CipherUtil.java
@@ -1,5 +1,6 @@
 package com.univ.initializer.util;
 
+import com.univ.initializer.bo.RSAKeyPO;
 import java.nio.charset.StandardCharsets;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
@@ -8,12 +9,11 @@ import java.security.KeyPairGenerator;
 import java.security.NoSuchAlgorithmException;
 import java.security.PrivateKey;
 import java.security.PublicKey;
-import java.security.interfaces.RSAPrivateKey;
-import java.security.interfaces.RSAPublicKey;
 import java.security.spec.EncodedKeySpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Arrays;
 import java.util.Base64;
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
@@ -21,41 +21,94 @@ import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
 
 /**
+ * base64编码、解码；
+ * RSA加密、解密；
+ *
  * @author univ 2023/2/24 15:06
  */
 public class CipherUtil {
 
 	/**
-	 * 十分重要！！！：只能对普通字符串作base64，不要把字节先转成string然后再编码，此时解码时得到的不是原字节。
-	 * 所以需要提供对byte进行编码的重载方法，用来直接对byte进行编码；
+	 * 将string形式进行base64编码。底层其实还是对此string对应的byte[]进行操作
+	 *
+	 * 十分重要！！！：base64编码、解码的入参都是byte[]；
+	 *
+	 * 如果输入是普通字符串，则先转成byte[]再编码、解码，
+	 * 但要注意！如果输入是byte[]，此时不要把byte[]先转成string然后再编码，直接编码、解码即可。
+	 * 	(编码后长度会变，但有些算法如RSA对输入的长度限制，融入base64时使用不当易出错)
+	 *
+	 * 所以需要提供对byte[]进行编码的重载方法，用来直接对byte[]进行编码；
+	 *
 	 * @param text
 	 * @return
 	 */
 	public static String base64Encode(String text) {
-		return Base64.getEncoder().encodeToString(text.getBytes(StandardCharsets.UTF_8));
+		return base64Encode(text.getBytes(StandardCharsets.UTF_8));
 	}
 
+	/**
+	 * 将byte[]形式进行base64编码
+	 * @param bytes
+	 * @return
+	 */
 	public static String base64Encode(byte[] bytes) {
 		return Base64.getEncoder().encodeToString(bytes);
 	}
 
+	/**
+	 * 将base64Text运用base64解码成String形式
+	 * @param base64Text
+	 * @return
+	 */
 	public static String base64Decode(String base64Text) {
 		return new String(base64DecodeToBytes(base64Text), StandardCharsets.UTF_8);
 	}
 
+	/**
+	 * 将base64Text运用base64解码成byte[]形式
+	 * @param base64Text
+	 * @return
+	 */
 	public static byte[] base64DecodeToBytes(String base64Text) {
 		return Base64.getDecoder().decode(base64Text);
 	}
 
-	public static void main(String[] args)
-			throws IllegalBlockSizeException, InvalidKeyException, BadPaddingException, NoSuchAlgorithmException, NoSuchPaddingException {
-		String text = "[B@2cfb4a64";
+	public static void main(String[] args) {
+		byte[] arr = {10, 11, 12, 13};
+		System.out.println(Arrays.toString(arr));
+		String str = new String(arr);
+		System.out.println(str);
+		String encode1 = base64Encode(str);
+		System.out.println(encode1);
+
+		String s = base64Decode(encode1);
+		System.out.println(s);
+		System.out.println(Arrays.toString(s.getBytes()));
+
+		// base64编码
+		/*String text = "[B@2cfb4a64";
 		String encode = base64Encode(text);
 		System.out.println(encode);
-		System.out.println(base64Decode(encode));
+		System.out.println(base64Decode(encode));*/
+
+		// RSA加解密
+		/*RSAKeyPO rsaKeyPO = generateRSAKey();
+		String encrypted = rsaEncrypt(rsaKeyPO.getPublicKey(), "fjeia都是没用工二月份");
+		System.out.println(encrypted);
+		String decrypted = rsaDecrypt(rsaKeyPO.getPrivateKey(), encrypted);
+		System.out.println(decrypted);*/
 	}
 
-	public void rsaDetail()
+	/**
+	 * RSA完整流程的示例
+	 *
+	 * @throws NoSuchAlgorithmException
+	 * @throws NoSuchPaddingException
+	 * @throws InvalidKeyException
+	 * @throws BadPaddingException
+	 * @throws IllegalBlockSizeException
+	 */
+	public static void rsaDetail()
 			throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
 		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
 		generator.initialize(2048);
@@ -66,47 +119,54 @@ public class CipherUtil {
 		String text = "fjieafe";
 		Cipher encryptCipher = Cipher.getInstance("RSA");
 		encryptCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+		// 重点1：加密的对象的是byte[]，解密的结果也是byte[]
 		byte[] encryptedBytes = encryptCipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
 
-		String byteStr = new String(encryptedBytes, StandardCharsets.UTF_8);
-
-		System.out.println("==============");
 		// 加密后的结果(byte)以base64编码，这样更易读
-		String encoded = base64Encode(byteStr);
+		String secretMsgWithBase64 = base64Encode(encryptedBytes);
+		System.out.println("RSA加密且base64编码后：" + secretMsgWithBase64);
 
-
+		// 解密
 		Cipher encryptCipher1 = Cipher.getInstance("RSA");
 		encryptCipher1.init(Cipher.DECRYPT_MODE, privateKey);
-		// 特别注意，这里必须使用encryptedBytes，是对string解码，而不是对byte解码
+		// 重点2：解密的对象的是byte[]，加密的结果也是byte[]
 		byte[] bytes = encryptCipher1.doFinal(encryptedBytes);
 		String decoded = new String(bytes, StandardCharsets.UTF_8);
-		System.out.println("RSA 解码后的byte[]: ---》 str: " + decoded);
-
+		System.out.println("RSA解码后的原文为: " + decoded);
 	}
 
-	public void generateRSAKey() throws NoSuchAlgorithmException {
-		KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA");
+	/**
+	 * 创建RSA公钥与私钥
+	 *
+	 * @throws NoSuchAlgorithmException
+	 */
+	public static RSAKeyPO generateRSAKey() {
+		KeyPairGenerator generator = null;
+		try {
+			generator = KeyPairGenerator.getInstance("RSA");
+		} catch (NoSuchAlgorithmException e) {
+			System.out.println("生成RSA密钥失败");
+			e.printStackTrace();
+		}
 		generator.initialize(2048);
 		KeyPair pair = generator.generateKeyPair();
 		PrivateKey privateKey = pair.getPrivate();
 		PublicKey publicKey = pair.getPublic();
-		System.out.println("publicKey: " + publicKeyToBase64Str(publicKey));
-		System.out.println("privateKey: " + privateKeyToBase64Str(privateKey));
-
-
+		return new RSAKeyPO(publicKeyToBase64Str(publicKey), privateKeyToBase64Str(privateKey));
 	}
-	public PublicKey base64StrToPublicKey(String base64Str)
+
+	public static PublicKey base64StrToPublicKey(String base64Str)
 			throws NoSuchAlgorithmException, InvalidKeySpecException {
 		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 		EncodedKeySpec publicKeySpec = new X509EncodedKeySpec(base64DecodeToBytes(base64Str));
 		return keyFactory.generatePublic(publicKeySpec);
 	}
 
-	public String publicKeyToBase64Str(PublicKey publicKey) {
+	public static String publicKeyToBase64Str(PublicKey publicKey) {
 		return base64Encode(publicKey.getEncoded());
 	}
 
-	public PrivateKey base64StrToPrivateKey(String base64Str)
+	public static PrivateKey base64StrToPrivateKey(String base64Str)
 			throws NoSuchAlgorithmException, InvalidKeySpecException {
 		KeyFactory keyFactory = KeyFactory.getInstance("RSA");
 		// 这里不能使用X509EncodedKeySpec
@@ -114,34 +174,63 @@ public class CipherUtil {
 		return keyFactory.generatePrivate(privateKeySpec);
 	}
 
-	public String privateKeyToBase64Str(PrivateKey privateKey) {
+	public static String privateKeyToBase64Str(PrivateKey privateKey) {
 		return base64Encode(privateKey.getEncoded());
 	}
 
-	public void rsaEncrypt() throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, InvalidKeySpecException {
-		String publicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAunH7sBeHIUZi3FUEMTNnDgpr+PIOTb5dsGYqWI9WDqQ3NEU5xlThb/kimknB9GVX6ZwKPIVK8L4GoCbYk7zhyy+Z869VOcbLLJ8CTCAUXKodxekMX2bC9rzyysbbOq0WN7FYg6R/u+SKlljgbjEhr6/RKNjIVTUtniWefSHhc75lqM4ImUzEZiVuWNBfiliUJRmuBJyzaN34RS9p1Nxgk7NTAO6AqeeoR2WIpN1CbQMLdBX7Fjk/HiJWB9aoBCYJTVw1SNInnUXMxDSzAy8Mt+LiMd+VQn+bXNWbZ1716afUWhXqfkmftLfBbLDsgne6V+hQH0D2FvXkxZhiGTpyMwIDAQAB";
-		String text = "hello, world";
-		Cipher encryptCipher = Cipher.getInstance("RSA");
-		encryptCipher.init(Cipher.ENCRYPT_MODE, base64StrToPublicKey(publicKey));
-		byte[] encryptedBytes = encryptCipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+	/**
+	 * RSA加密
+	 *
+	 * 注意：RSA加密以及解密的对象都是byte[]，在对结果应用base64时注意使用byte[]形式而不是字符串形式
+	 * @return
+	 */
+	public static String rsaEncrypt(String publicKey, String text)  {
+		Cipher encryptCipher = null;
+		try {
+			encryptCipher = Cipher.getInstance("RSA");
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+			e.printStackTrace();
+		}
+		try {
+			encryptCipher.init(Cipher.ENCRYPT_MODE, base64StrToPublicKey(publicKey));
+		} catch (InvalidKeyException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+			e.printStackTrace();
+		}
+		byte[] encryptedBytes = new byte[0];
+		try {
+			encryptedBytes = encryptCipher.doFinal(text.getBytes(StandardCharsets.UTF_8));
+		} catch (IllegalBlockSizeException | BadPaddingException e) {
+			e.printStackTrace();
+		}
 
 		// 加密后的结果(byte)以base64编码，这样更易读
-		String encoded = base64Encode(encryptedBytes);
-		System.out.println(encoded);
+		return base64Encode(encryptedBytes);
 	}
 
-	public void rasDecrypt()
-			throws NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeySpecException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException {
-		String privateKey = "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC6cfuwF4chRmLcVQQxM2cOCmv48g5Nvl2wZipYj1YOpDc0RTnGVOFv+SKaScH0ZVfpnAo8hUrwvgagJtiTvOHLL5nzr1U5xsssnwJMIBRcqh3F6QxfZsL2vPLKxts6rRY3sViDpH+75IqWWOBuMSGvr9Eo2MhVNS2eJZ59IeFzvmWozgiZTMRmJW5Y0F+KWJQlGa4EnLNo3fhFL2nU3GCTs1MA7oCp56hHZYik3UJtAwt0FfsWOT8eIlYH1qgEJglNXDVI0iedRczENLMDLwy34uIx35VCf5tc1ZtnXvXpp9RaFep+SZ+0t8FssOyCd7pX6FAfQPYW9eTFmGIZOnIzAgMBAAECggEAU46fyKRSuDPuPGaRkWdDCUTcbN42UNs+675tvfDHFZq87t0TbnI98kvKGTFzDg+ZHaYD5GmsU0YmfAHM0DE3VDNKIK70WAdiqOW+RtWdqoXHABpYpCzOtXuqp/wE5kAKE/2T+LNxV4iuH4FHW7sDKNR3vYGiDqXdguHDLLtRaegndBlPuoku4E2+hYH0voA4e1+s11bQhL10ANuZbSZwBEGZ9IvXF6CH8t6eXo/TnmBMiek+7Q/m6l586ttk9syND7BMEldqX+c/ft6dCcDJKoiJFDYsKA4tZKGbb6fyK8GRZXz3aEEXZ8NN/3JoPKb6alVdtiMiUhVKJL1SQeoPQQKBgQDjpP0NiZVNaDofWbLL1YhqnHUe2+wdWpZLa8iRKjgNeGIRs7ab0qhK4n23l08AvwEgf1RC+1zH/UaH732hbshLR7QCm1yTXgdG9kRxKi2YVcd8y+JoW6iZj53xX9AW0LynFFjmuLGkGTKbNx3HymVL4UZ4P8BZljIdbV9Cwk5ykwKBgQDRq0EY3yj/M9lPt8C47b8zbheeSjk6uU6iAy5u84/SrYzn9eTKCyM7ppXArIJXETS3QllhYJLrVCqqaJFeNpd8zmlQQ1qszmILwsftwHH/UtAZ61n9XvZEiJq9QUFVsOCIfpKe4ZyNk7ROKnwBR2fz/bULXP9z1PTvduiNDjWl4QKBgDMM5/bmOMZALnCjY/ZUL6CA77744M2AaB4H0NXFGwbiTVPc8oGlTzpj6n+GfiATtYP9Kf9PcXIj+XI4ofrW5jIDpXnEL5GYz5b5WUXriIgWDIOPEXoHvBqb23aq3vaO3w+0/27WUngTcobeaF5KpEbNAUmsOk2c4iY5RdipzH/jAoGBALR1uK7GjEuy/axeVLMWdgW3EP79lWpUyjgpktKOMdC77VZMzuGTWnzh8WyHThvQRFYi9jkY3PtNUveeztePFy2lnwElJ6sqazsCKAAbLkL6rQBSD/9Ct6XfCYgw3SH6/hadEmdLeeplIcBRUq/rHK7lQ+kG3xt8RGDDMH8Ppt8hAoGAaOitKMrjiPK3qd0yMRBQ5Y1isu2/OKFjtLRMvJZRaMSeTPWQ+kpWXFdyaTady/kCbbSfOnucbz6mJoxt017a6MqPpaxwIK1FQtnkeEX0fjTs8gwgdkfgNOHIlaWldG0RDuBSaEomhnd10XojfugI6QP0dDaEpZmKjLogkTgBGJY=";
-		String secretMsg = "UhCzN4pbSpb1h5FtQDuAUQ1kAO1yZghhrMag4wosLmbq0wW7w0m42cvNobXtrLHVmu3xbTQtWKvnGZofbzyuiuhbdUcJWeQshASHgKjCLTR8WLofsvXeo/8X18Itt6EXlM4usg793+kwRwpeQz3rivA3xk9+V5SQYRrxnMxZA77dY3D/DWVKybT2K3HxDiiSqKebTIK6oeuIcsLutKydtC3Do96/FdS6rWwa0zgCvG+4P486cja/oUyx2e/7g3MEajcJdOjP91Qisd9PGGG6QJZicxZMOQrIZMlvnkH9SiVDVmPrNStFiMU9oeoHoURo1jjX/r0NvjfnJkEkFcKlBA==";
-		Cipher decryptCipher = Cipher.getInstance("RSA");
-//		System.out.println(privateKeyToBase64Str(base64StrToPrivateKey(privateKey)));;
-		decryptCipher.init(Cipher.DECRYPT_MODE, base64StrToPrivateKey(privateKey));
-		// 特别注意，这里必须使用encryptedBytes，是对string解码，而不是对byte解码
-		byte[] bytes = decryptCipher.doFinal(base64DecodeToBytes(secretMsg));
-		System.out.println(new String(bytes, StandardCharsets.UTF_8));
+	/**
+	 * RSA解密
+	 * 注意：RSA加密以及解密的对象都是byte[]，在对结果应用base64时注意使用byte[]形式而不是字符串形式
+	 */
+	public static String rsaDecrypt(String privateKey, String secretMsg) {
+		Cipher decryptCipher = null;
+		try {
+			decryptCipher = Cipher.getInstance("RSA");
+		} catch (NoSuchAlgorithmException | NoSuchPaddingException e) {
+			e.printStackTrace();
+		}
+		try {
+			decryptCipher.init(Cipher.DECRYPT_MODE, base64StrToPrivateKey(privateKey));
+		} catch (InvalidKeyException | InvalidKeySpecException | NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		// 特别注意，这里base64解码必须使用byte[]的返回值形式
+		byte[] bytes = new byte[0];
+		try {
+			bytes = decryptCipher.doFinal(base64DecodeToBytes(secretMsg));
+		} catch (IllegalBlockSizeException | BadPaddingException e) {
+			e.printStackTrace();
+		}
+		return new String(bytes, StandardCharsets.UTF_8);
 	}
-
-
 
 }
