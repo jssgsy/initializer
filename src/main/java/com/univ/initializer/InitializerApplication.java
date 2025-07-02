5e6be34074b1b9df52f302b5cfcd2868120463d2
univ <fcsnwu@163.com>
2025-07-02 14:55:17 +0800

    添加启动成功日志、日志配置优化

--- src/main/java/com/univ/initializer/InitializerApplication.java
+++ src/main/java/com/univ/initializer/InitializerApplication.java
@@ -1,10 +1,16 @@
 package com.univ.initializer;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 /**
  * @author univ
@@ -15,8 +21,19 @@ import org.springframework.cache.annotation.EnableCaching;
 // 多数据源的情况下，这里就不用再配置了
 //@MapperScan({"com.univ.initializer.mapper"})
 @EnableCaching
+@Slf4j
 public class InitializerApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(InitializerApplication.class, args);
+    public static void main(String[] args) throws UnknownHostException {
+        ConfigurableApplicationContext application = SpringApplication.run(InitializerApplication.class, args);
+        Environment env = application.getEnvironment();
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        String port = env.getProperty("server.port");
+        String path = env.getProperty("server.servlet.context-path");
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application InitializerApplication is running! Access URLs:\n\t" +
+                "Local: \t\thttp://localhost:" + port + path + "/\n\t" +
+                "External: \thttp://" + ip + ":" + port + path + "/\n\t" +
+                "Swagger文档: \thttp://" + ip + ":" + port + path + "/doc.html\n" +
+                "----------------------------------------------------------");
     }
 }
